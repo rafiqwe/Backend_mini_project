ba8f6e5f8fc18e05a5bdc842a7e848900da2b65c
rafiqwe <rmlmanik502@gmail.com>
2025-06-12 17:48:39 +0600

    Create login, logout route and write code or create post model, login.ejs page

--- Models/user.js
+++ Models/user.js
@@ -8,6 +8,12 @@ const userSchema = mongoose.Schema({
     age: Number,
     email: String,
     password: String,
+    post : [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref:"post"
+        }
+    ]
 });
 
 module.exports = mongoose.model('user', userSchema);
--- app.js
+++ app.js
@@ -1,20 +1,64 @@
-const express = require('express');
-const userSchema = require("./Models/user");
-const cookieParser = require('cookie-parser');
+const express = require("express");
+const userModel = require("./Models/user");
+const cookieParser = require("cookie-parser");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 const app = express();
 
 app.set("view engine", "ejs");
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 const port = 3000;
 
 app.get("/", (_, res) => {
-    res.render("index");
+  res.render("index");
+});
+app.get("/login", (_, res) => {
+  res.render("login");
+});
+
+app.post("/register", async (req, res) => {
+  const { username, name, age, email, password } = req.body;
+  const user = await userModel.findOne({ email });
+  if (user) res.status(500).send("User already Registered");
+
+  bcrypt.genSalt(10, (err, salt) => {
+    bcrypt.hash(password, salt, async (err, hash) => {
+      const user = await userModel.create({
+        username,
+        name,
+        age,
+        email,
+        password: hash,
+      });
+
+      const token = jwt.sign({ email, userId: user._id }, "hahahah");
+      res.cookie("token", token);
+      res.send("Registered");
+    });
+  });
+});
+
+app.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+  const user = await userModel.findOne({ email });
+  if (!user) res.status(500).send("Somthing Going Wrong!");
+
+  bcrypt.compare(password, user.password, (err, result) => {
+    if (result) {
+      const token = jwt.sign({ email, userId: user._id }, "hahahah");
+      res.cookie("token", token);
+      res.status(200).send("You can login");
+    } else res.redirect("/login");
+  });
+});
+
+app.get("/logout", (_, res) => {
+  res.cookie("token", "");
+  res.redirect("/login");
 });
 
 app.listen(port);
-
-
