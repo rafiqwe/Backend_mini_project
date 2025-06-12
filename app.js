455c3a368caed66a47e72604c681fbca2e0165b9
rafiqwe <rmlmanik502@gmail.com>
2025-06-12 14:28:53 +0600

    Create register from only frontend

--- app.js
+++ app.js
@@ -2,12 +2,13 @@ const express = require('express');
 const userSchema = require("./Models/user");
 const cookieParser = require('cookie-parser');
 
+const app = express();
+
 app.set("view engine", "ejs");
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 
-const app = express();
 const port = 3000;
 
 app.get("/", (_, res) => {
