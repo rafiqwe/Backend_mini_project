335bfdb80003b09acbc9b13b06a41eebed31333e
rafiqwe <rmlmanik502@gmail.com>
2025-06-12 14:20:08 +0600

    create user model and setup meddelware

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 const express = require('express');
+const userSchema = require("./Models/user");
+const cookieParser = require('cookie-parser');
+
+app.set("view engine", "ejs");
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(cookieParser());
 
 const app = express();
 const port = 3000;
 
 app.get("/", (_, res) => {
-    res.send("Hello World");
+    res.render("index");
 });
 
 app.listen(port);
